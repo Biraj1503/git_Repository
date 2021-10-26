6a33e13785b63d2dbf33fdfb9fa5c882d815a18d
Biraj Kumar Biswas <toshebamail@gmail.com>
2021-10-26 14:42:06 +0600

    Add Few Line BranchA

--- git.js
+++ git.js
@@ -2,3 +2,14 @@ let git = "WoW! This is First Time Worked Git & Github. My Fellings Really Amazi
 console.log(git)
 let arr = [1,2,3]
 console,log(arr)
+
+module.exports ={
+  dev:{
+    name:'Biraj Kumar Biswas',
+    email:"toshebamail@gmail.com"
+  },
+  pro:{
+    name:'Shathi Rani Talukder',
+    email:"shathirani@gmail.com"
+  }
+}
