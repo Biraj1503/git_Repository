505ff4079613d4136f2fd4b8d3e4a0a17b2f7556
Biraj Kumar Biswas <toshebamail@gmail.com>
2021-10-26 16:10:37 +0600

    Add some code branchB

--- git.js
+++ git.js
@@ -2,3 +2,23 @@ let git = "WoW! This is First Time Worked Git & Github. My Fellings Really Amazi
 console.log(git)
 let arr = [1,2,3]
 console,log(arr)
+
+let branchB = [
+		{
+			name:'git',
+			address:'git.com',
+			age:18
+		},
+		
+		{
+			name:'github',
+			address:'github.com',
+			age:20
+		},
+		
+		{
+			name:'Facebook',
+			address:'facebook.com',
+			age:20
+		}
+]
